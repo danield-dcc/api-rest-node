bca80855fd1185f06b5f57ceac8867fee99b8606
Daniel <danieldcc.colares@gmail.com>
2023-08-07 20:05:58 -0300

    🚨 render host port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { app } from './app'
 import { env } from './env'
 
+const port = env.PORT
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+
 app
   .listen({
-    port: env.PORT,
+    host,
+    port,
   })
   .then(function () {
     console.log('HTTP Server Running on port 3333!')
